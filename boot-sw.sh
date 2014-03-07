90e246495e0a32ee766527f9f4a1456ec6f5147e
Hilmar Lapp <hlapp@drycafe.net>
2014-03-06 22:08:26 -0500

    Change out pyenv to virtualenv.
    At least that works, even if pyenv would be nicer.

--- boot-sw.sh
+++ boot-sw.sh
@@ -8,6 +8,7 @@ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 echo "Installing pip package manager"
 curl -fsSL https://raw.github.com/pypa/pip/master/contrib/get-pip.py --output get-pip.py
 sudo python get-pip.py
+sudo pip install virtualenv
 
 # installing brew formulae
 for pkg in `cat brew-formulae.txt` ; do
@@ -15,13 +16,22 @@ for pkg in `cat brew-formulae.txt` ; do
 done
 
 # create python virtual envs
+echo "Creating Python virtual environments"
+
+# directory holding virtualenvs
+pyenv_dir=$HOME/.pyenv
+test -d $pyenv_dir || mkdir $pyenv_dir
+
 for venv in `ls requirements-*.txt | perl -p -e 's/.*-(\w+)\.txt/$1/;'` ; do
-    pyenv virtualenv $venv
+    echo "########### $venv"
+    (cd $pyenv_dir; virtualenv $venv)
 done
 
 # install packages needed in each virtual env
+echo "Installing package requirements for Python virtual environments" 
+
 for venv in `ls requirements-*.txt | perl -p -e 's/.*-(\w+)\.txt/$1/;'` ; do
-    pyenv activate $venv
+    echo "########### $venv"
+    . $pyenv_dir/$venv/bin/activate
     pip install -r requirements-$venv.txt
-    pyenv deactivate $venv
 done
